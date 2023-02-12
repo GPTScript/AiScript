c64d8181013de8665a5796e1ccdad09a87b23eee
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2023-02-12 14:13:24 +0100

    bootstrap project

---
+++ experiment-0/webpack.config.js
@@ -0,0 +1,47 @@
+import * as path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const config = {
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: [
+                    /node_modules/,
+                    path.resolve(__dirname, 'test')
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+        fallback: {
+            fs: false,
+            path: false
+        }
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: {
+            type: "module"
+        }
+    },
+    devtool: "source-map",
+    experiments: {
+        outputModule: true
+    },
+    performance: {
+        maxAssetSize: 512000,
+        maxEntrypointSize: 512000
+    },
+
+}
+
+export default config;
+
